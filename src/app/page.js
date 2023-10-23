670e66a0f3eb1a40b7a8b243fa0750793cc4c460
Raphael Gatchalian <rlgatchalian@gmail.com>
2023-10-22 19:07:09 -0700

    Added lastfm fetching functionality

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
-import Image from 'next/image'
+import LastfmData from '@/components/lastfm-data';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hey
+    <main className='flex min-h-screen flex-col items-center justify-center p-24'>
+      <h1 className='text-xl font-bold'>myhitboard</h1>
+      {/* Right now only my personal lastfm is being fetched. Of course this wiill be changed soon*/}
+      <LastfmData
+        username={'turntablesturn'}
+        apiKey={process.env.LASTFM_API_KEY} />
     </main>
-  )
+  );
 }
