0faf7ca977d3e60be0a948da3831b8ed9fbc202d
Raphael Gatchalian <rlgatchalian@gmail.com>
2023-10-23 01:59:43 -0700

    Added input validation + formatting tweaks

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,7 @@ import LastfmFlow from '@/components/LastfmFlow';
 export default function Home() {
   return (
     <main className='flex max-h-full flex-col items-center justify-center p-24'>
-      <h1 className='text-3xl font-bold pb-5'>
-        myhitboard
-      </h1>
-      
+      <h1 className='text-3xl font-bold pb-5'>myhitboard</h1>
       <LastfmFlow />
     </main>
   );
--- src/components/LastfmForm.js
+++ src/components/LastfmForm.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 
 export default function LastfmForm({ onSubmit }) {
   const [username, setUsername] = useState('');
+  const [inputError, setInputError] = useState(true);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -11,15 +12,18 @@ export default function LastfmForm({ onSubmit }) {
   }
 
   function handleChange(e) {
-    e.preventDefault();
-    setUsername(e.target.value);
+    const inputValue = e.target.value;
+    setUsername(inputValue);
+    if (inputValue.length <= 15) {
+      setInputError(''); // Clear the error message when the input changes
+    } else {
+      setInputError('Username must be 15 characters or less');
+    }
   }
 
   return (
-    <div className='w-full max-w-xs'>
-      <form
-        onSubmit={handleSubmit}
-        className='rounded bg-slate-100 px-8 pt-6 pb-8 mb-4'>
+    <div className='rounded max-w-md bg-slate-100 px-8 pt-6 pb-8 mb-4'>
+      <form onSubmit={handleSubmit}>
         <div className='mb-4'>
           <label
             className='block text-gray-700 text-sm font-bold mb-2'
@@ -33,10 +37,13 @@ export default function LastfmForm({ onSubmit }) {
             onChange={handleChange}
             className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' 
           />
+          <span class='flex items-center font-medium text-red-500 text-xs mt-2 ml-1'>
+			      {inputError}
+		      </span>
         </div>
         <div className='flex items-center justify-between'>
           <button
-            disabled={!username}
+            disabled={!username || username.length > 15}
             type='submit'
             className='mx-auto bg-red-500 hover:bg-red-700 disabled:bg-red-300 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'>
             See Top 10
